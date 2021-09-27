2d0667117e5d6f1f427fade738ec640d58fcc5d4
xuetongzh <84268870+xuetongzh@users.noreply.github.com>
2021-09-28 00:49:18 +1000

    Add files via upload

---
+++ ContactUs.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <h5>Contact Us</h5>
+    <div class="contact-us-item">
+      <span class="material-icons material-icons-outlined"> person </span>
+      Xuetong Zhao
+    </div>
+    <div class="contact-us-item">
+      <span class="material-icons material-icons-outlined"> email </span
+      >zhaoxuet@deakin.edu.au
+    </div>
+  </footer>
+</template>
